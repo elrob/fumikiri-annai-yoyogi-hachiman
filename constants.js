d8810ef88d5eb662934127569f45f97f3497678f
Robert Speller <robert.speller@smartnews.com>
2021-05-05 11:22:18 +0900

    First experimental data preparation scripts

---
+++ constants.js
@@ -0,0 +1,6 @@
+const SANGUBASHI = 'Odakyu.Odawara.Sangubashi';
+const SHINJUKU = 'Odakyu.Odawara.Shinjuku';
+const YOYOGI_HACHIMAN = 'Odakyu.Odawara.YoyogiHachiman';
+const YOYOGI_UEHARA = 'Odakyu.Odawara.YoyogiUehara';
+
+module.exports = {SANGUBASHI, SHINJUKU, YOYOGI_HACHIMAN, YOYOGI_UEHARA};
