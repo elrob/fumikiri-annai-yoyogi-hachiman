1a1cd9e401032a0ac2f5a26a2a8743d9afa42548
Robert Speller <robert.speller@smartnews.com>
2021-05-05 21:49:43 +0900

    Dynamic UI with updated timetables

--- extract.js
+++ extract.js
@@ -1,34 +1,45 @@
 const {readFileSync, writeFileSync} = require('fs');
 const {SHINJUKU} = require('./constants');
 
-const originalTimetable = JSON.parse(readFileSync('timetable-holiday.json', 'utf8'));
-
-const repairSuperHakoneShinjukuDepartureTimes = {
-    'Super Hakone 5': '09:00',
-    'Super Hakone 7': '10:00',
-    'Super Hakone 9': '10:20',
-    'Super Hakone 11': '11:00'
-};
-
-const repair = train => {
-    const {tt, nm} = train;
-    const {s, a, d} = tt && tt.find(({s}) => s === SHINJUKU) || {};
-    if (s && !a && !d) {
-        const [{en}] = nm;
-        const departureTime = repairSuperHakoneShinjukuDepartureTimes[en];
-        const repairedTimetable =
-            tt.map(stationTime => stationTime.s === SHINJUKU ? {...stationTime, d: departureTime} : stationTime);
-        const afterRepair = {...train, tt: repairedTimetable};
-        console.log(JSON.stringify({beforeRepair: train, afterRepair}, null, 2));
-        return afterRepair;
+const SUPER_HAKONE_SHINKUKU_DEPARTURE_TIMES = {
+    weekday: {
+        'Super Hakone 7': '10:00',
+        'Super Hakone 9': '11:00'
+    },
+    holiday: {
+        'Super Hakone 5': '09:00',
+        'Super Hakone 7': '10:00',
+        'Super Hakone 9': '10:20',
+        'Super Hakone 11': '11:00'
     }
-    return train;
 };
-const relevantTimetable = originalTimetable
-    .filter(({tt}) => tt && tt.find(({s}) => s === SHINJUKU))
-    .map(repair);
 
-const relevantTimetableJsonString = JSON.stringify(relevantTimetable, null, 2);
+[
+    ['timetable-weekday.json', SUPER_HAKONE_SHINKUKU_DEPARTURE_TIMES.weekday],
+    ['timetable-holiday.json', SUPER_HAKONE_SHINKUKU_DEPARTURE_TIMES.holiday]
+].forEach(([originalTimetableFileName, superHakoneShinjukuDepartureTimes]) => {
+    const originalTimetable = JSON.parse(readFileSync(originalTimetableFileName, 'utf8'));
 
-writeFileSync('relevant-timetable-holiday.json', relevantTimetableJsonString);
+    const repair = train => {
+        const {tt, nm} = train;
+        const {s, a, d} = tt && tt.find(({s}) => s === SHINJUKU) || {};
+        if (s && !a && !d) {
+            const [{en}] = nm;
+            const departureTime = superHakoneShinjukuDepartureTimes[en];
+            const repairedTimetable =
+                tt.map(stationTime => stationTime.s === SHINJUKU ? {...stationTime, d: departureTime} : stationTime);
+            const afterRepair = {...train, tt: repairedTimetable};
+            console.log(JSON.stringify({beforeRepair: train, afterRepair}, null, 2));
+            return afterRepair;
+        }
+        return train;
+    };
+    const relevantTimetable = originalTimetable
+        .filter(({tt}) => tt && tt.find(({s}) => s === SHINJUKU))
+        .map(repair);
+
+    const relevantTimetableJsonString = JSON.stringify(relevantTimetable, null, 2);
+
+    writeFileSync(`relevant-${originalTimetableFileName}`, relevantTimetableJsonString);
+})
 
--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,76 @@
 <template>
   <header>
     <h1>踏切案内 代々木八幡</h1>
-    {{ displayTime }}
+    <span class="header-2">{{ displayTime }}</span>
+    <span class="header-2">{{ currentState ? 'Closed' : 'Open' }}</span>
   </header>
-  <ul id="timeline">
-    <li
-        v-for="(state, index) in states"
-        :key="index"
-        class="state-panel"
-        :class="state.overallState ? 'closed' : 'open'"
-        :style="{ height: (0.15 * state.durationSeconds)  + 'em' }"
-    >
-      {{ new Date(state.time).toLocaleTimeString('ja-JP') }}
-    </li>
-  </ul>
+  <main id="timeline">
+    <hr id="now-marker">
+    <ul id="open-close-times-list">
+      <li
+          v-for="(openCloseTime, index) in openCloseTimes"
+          :key="openCloseTime.time"
+          class="state-panel"
+          :class="openCloseTime.overallState ? 'closed' : 'open'"
+          :style="{ height: (0.1 * openCloseTime.durationSeconds)  + 'em' }"
+      >
+        {{ index !== 0 ? new Date(openCloseTime.time).toLocaleTimeString('ja-JP') : null }}
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
-import resolvedTimeline from '../relevant-trains-now.json'
+import openCloseTimes from '../open-close-times-holiday.json'
+
+const secondsToMillis = seconds => seconds * 1000;
+const millisToSeconds = millis => Math.floor(millis / 1000);
 
 const getDisplayableCurrentTime = () => new Date().toLocaleTimeString('ja-JP')
+
+const getCurrentStateAndTimeline = () => {
+  const now = secondsToMillis(millisToSeconds(Date.now()));
+  const lowerBound = now - secondsToMillis(30);
+  const upperBound = now + secondsToMillis(3600);
+  const filteredOpenCloseTimes = openCloseTimes
+      .filter(({durationSeconds}) => durationSeconds > 0)
+      .filter(({time}, index, times) =>
+          time >= lowerBound && time < upperBound
+          || (times[index + 1] && times[index + 1].time >= lowerBound && times[index + 1].time < upperBound))
+      .map((openCloseTime, index, times) => index === 0 && times[index + 1]
+          ? {...openCloseTime, durationSeconds: millisToSeconds(times[index + 1].time - lowerBound)}
+          : openCloseTime);
+  const [one, two] = filteredOpenCloseTimes;
+  console.log(JSON.stringify({one, two}));
+  const currentState = openCloseTimes.find(({time}, index, times) => time <= now && times[index + 1].time > now)?.overallState;
+  return {openCloseTimes: filteredOpenCloseTimes, currentState};
+}
+
 
 export default {
   name: 'App',
   data() {
+    const {openCloseTimes, currentState} = getCurrentStateAndTimeline();
     return {
-      interval: null,
+      clockInterval: null,
       displayTime: getDisplayableCurrentTime(),
-      states: resolvedTimeline.filter(({durationSeconds}) => durationSeconds > 0)
+      openCloseTimes,
+      currentState
     }
   },
   beforeUnmount() {
-    clearInterval(this.interval)
+    clearInterval(this.clockInterval)
   },
   created() {
     const that = this;
-    this.interval = setInterval(() => {
+    this.clockInterval = setInterval(() => {
       that.displayTime = getDisplayableCurrentTime()
     }, 1000);
+    this.refreshDisplayInterval = setInterval(() => {
+      const {openCloseTimes, currentState} = getCurrentStateAndTimeline();
+      that.currentState = currentState;
+      that.openCloseTimes = openCloseTimes;
+    }, 1000)
   }
 }
 </script>
@@ -68,15 +101,34 @@ header > h1 {
   letter-spacing: 0.1em;
 }
 
+.header-2 {
+  display: inline-block;
+  width: 50%;
+}
+
 #timeline {
-  margin: 0;
   padding: 5em 0 0;
+}
+
+#now-marker {
+  height: 0.2em;
+  color: #082A5B;
+  background: #082A5B;
+  margin: 3em 0 0 0;
+  width: 100%;
+  position: fixed;
+}
+
+#open-close-times-list {
+  margin: 0;
+  padding: 0;
+  z-index: -1;
   list-style-type: none;
 }
 
 .state-panel {
   border-bottom: 0.35em solid #DEF7FF;
-  padding: 0.4em
+  padding-left: 0.4em
 }
 
 .open {
